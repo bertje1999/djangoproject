a33a123a86a1c606805f711d0bd9d1eda7de5fa9
bertje1999 <bertcoeckelberghs@gmail.com>
2023-01-05 13:52:15 +0100

    voetbalspelers model toegevoegd

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Post
+from .models import Voetbalspeler
 admin.site.register(Post)
-
+admin.site.register(Voetbalspeler)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,3 +15,17 @@ class Post(models.Model):
 
    def __str__(self):
        return self.title
+
+class Voetbalspeler(models.Model):
+   naam_van_de_voetballer = models.CharField(max_length=200)
+   actuele_voetbalclub = models.CharField(max_length=200)
+   naam_van_de_auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+   datum_van_invoer = models.DateTimeField(default=timezone.now)
+   datum_van_laatste_aanpassing = models.DateTimeField(blank=True, null=True)
+
+   def publish(self):
+       self.published_date = timezone.now()
+       self.save()
+
+   def __str__(self):
+       return self.naam_van_de_voetballer
