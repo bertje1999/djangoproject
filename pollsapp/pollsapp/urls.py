22dc1615ceb3b2aae231bf5300b3039cbf36af8f
bertje1999 <bertcoeckelberghs@gmail.com>
2023-01-13 18:50:39 +0100

    afgewerkt

--- pollsapp/pollsapp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('pollsapp_app.urls')),
-]
--- pollsapp/pollsapp_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PollsappAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pollsapp_app'
